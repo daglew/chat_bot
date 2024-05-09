5a50d36fcbc09e631c837bc90c96233cf14e974d
daglew <daga122611@gmail.com>
2024-05-09 22:49:37 +0200

    chat_bot_app

--- frontend/main.py
+++ frontend/main.py
@@ -1,5 +1,10 @@
 import gradio
 
-class GradioFrontend:
-    pass
+
+class ChatBotFrontend:
+    def __init__(self, title: str):
+        self.title = title
+
+    def open_demo(self, function):
+        return gradio.Interface(fn=function, inputs="text", outputs="text", title=self.title)
 
